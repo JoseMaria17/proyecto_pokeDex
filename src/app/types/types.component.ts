c5d3ff75eedde52e73b2ea1d706cc81e95c3925b
José María  González <zljosewark1a@gmail.com>
2022-12-07 14:51:20 -0500

    cambio ruta img

--- src/app/types/types.component.ts
+++ src/app/types/types.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit} from '@angular/core';
 import { DataService } from '../service/data.service';
 
+
 @Component({
   selector: 'app-types',
   templateUrl: './types.component.html',
@@ -8,7 +9,7 @@ import { DataService } from '../service/data.service';
 })
 export class TypesComponent implements OnInit{
   pokemons:any[]=[];
-
+ 
 
 
   constructor(
@@ -18,11 +19,11 @@ export class TypesComponent implements OnInit{
   ngOnInit(): void {
 
   }
-  
+
 
  typesPoke(typeSelect:any):void{
     this.pokemons=[]
-    
+
     /* let asBox=this.box.nativeElement */
     this.dataService.getPokemons()
     .subscribe((response:any)=>{
@@ -33,16 +34,16 @@ export class TypesComponent implements OnInit{
         .subscribe((response:any)=>{
           /* this.pokemons.push(response); */
           let type='';
-    
+
           response.types.forEach((element: any) => {
             type=type+element.type.name.charAt(0).toUpperCase()+element.type.name.slice(1)+' '
 
-           
+
           });
-          
+
           if(type.includes(typeSelect)) {
-              
-          
+
+
               this.pokemons.push({
               'name':response.name.charAt(0).toUpperCase()+response.name.slice(1),
               'img':response.sprites.front_default,
@@ -51,11 +52,11 @@ export class TypesComponent implements OnInit{
               'atack':response.stats[1].base_stat,
               'types':type});
             }
-         
+
         })
       });
     });
- 
+
 
 }
 }
