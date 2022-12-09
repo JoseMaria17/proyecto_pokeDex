0a4b36eda854ae8ffbadbf6c67096c5fdd2ee849
José María  González <zljosewark1a@gmail.com>
2022-12-09 15:09:11 -0500

    Last commit

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -10,7 +10,6 @@ import { DataService } from '../service/data.service';
 export class PokemonListComponent implements OnInit {
 
   pokemons:any[]=[];
-  page=1;
  /*  totalPokemons!: number; */
 
 
@@ -32,10 +31,44 @@ export class PokemonListComponent implements OnInit {
           .subscribe((response:any)=>{
             /* this.pokemons.push(response); */
             let type='';
-
+            let clase_fondo='';
             response.types.forEach((element: any) => {
               type=type+element.type.name.charAt(0).toUpperCase()+element.type.name.slice(1)+' '
             });
+
+            switch ( response.types[0].type.name) {
+              case 'fire':
+                clase_fondo='rojo'
+                break;
+              case 'water':
+                clase_fondo='azul'
+                break;
+              case 'grass':
+                clase_fondo='verde'
+                break
+              case 'poison':
+                clase_fondo='rosa'
+                break
+              case 'bug':
+                clase_fondo='negro'
+                break
+              case 'flying':
+                clase_fondo='azul__claro'
+                break
+              case 'normal':
+                clase_fondo='naranja'
+                break
+              case 'fairy':
+                clase_fondo='rosa__claro'
+                break
+              case 'psychic':
+                clase_fondo='verde__claro'
+                break
+              case 'electric':
+                clase_fondo='amarillo'
+                break
+            }
+
 
             this.pokemons.push({
                 'name':response.name.charAt(0).toUpperCase()+response.name.slice(1),
@@ -43,14 +76,16 @@ export class PokemonListComponent implements OnInit {
                 'height':response.height,
                 'health':response.stats[0].base_stat,
                 'atack':response.stats[1].base_stat,
-                'types':type});
+                'types':type ,
+                'fondo': clase_fondo});
 
 
           })
         });
       });
     }
+  }
 
-}
+
 
 
--- src/app/pokemon-search/pokemon-search.component.ts
+++ src/app/pokemon-search/pokemon-search.component.ts
@@ -80,10 +80,43 @@ export class PokemonSearchComponent implements OnInit{
         this.cantidad=this.cantidad+1;
 
         let type='';
+        let clase_fondo='';
         //guarda todos los tipos de los pokemones seleccionados ej Veneno, Aire
         d.types.forEach((element: any) => {
           type=type+element.type.name.charAt(0).toUpperCase()+element.type.name.slice(1)+' '
         });
+        switch ( d.types[0].type.name) {
+          case 'fire':
+            clase_fondo='rojo'
+            break;
+          case 'water':
+            clase_fondo='azul'
+            break;
+          case 'grass':
+            clase_fondo='verde'
+            break
+          case 'poison':
+            clase_fondo='rosa'
+            break
+          case 'bug':
+            clase_fondo='negro'
+            break
+          case 'flying':
+            clase_fondo='azul__claro'
+            break
+          case 'normal':
+            clase_fondo='naranja'
+            break
+          case 'fairy':
+            clase_fondo='rosa__claro'
+            break
+          case 'psychic':
+            clase_fondo='verde__claro'
+            break
+          case 'electric':
+            clase_fondo='amarillo'
+            break
+        }
 
         //se llena el arreglo para mostrar en el html
         this.pokemons_tj.unshift({
@@ -92,7 +125,8 @@ export class PokemonSearchComponent implements OnInit{
             'height':d.height,
             'health':d.stats[0].base_stat,
             'atack':d.stats[1].base_stat,
-            'types':type});
+            'types':type,
+            'fondo': clase_fondo});
 
         if(this.cantidad==6){
           //agrega clases nueva para la tarjeta y se indica que esta listo
